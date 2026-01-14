0590bd8051cf5f62c129482279a5d0ce88d874f0
jcylemonii <ckonorev2020@gmail.com>
2026-01-14 21:54:28 +0300

    Added simple functionality to handle content loading

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,5 +1,16 @@
+from utils.files import download_file
+from os.path import exists
+
 def main():
-    print("Hello, Python Practice 15!")
+
+    resource = "https://storage.yandexcloud.net/academy.ai/A_Z_Handwritten_Data.csv"
+    destination = "./src/datasets/A_Z_Handwritten_Data.csv"
+
+    if exists(destination) is False:
+        print(f"The file does not exist at {destination}")
+        download_file(resource, destination)
+    else:
+        print(f"The file already exists at {destination}")
 
 if __name__ == "__main__":
     main()
---
+++ src/utils/files.py
@@ -0,0 +1,30 @@
+import requests
+
+def download_file(url: str, destination: str) -> None:
+
+    print(f"Starting download from {url} to {destination}")
+
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    loaded_content = 0
+    content_length = response.headers.get("Content-Length")
+
+    if content_length is None:
+        print("Content length is not provided")
+        return
+    
+    content_length = int(content_length)
+
+    with open(destination, "wb") as file:
+        for chunk in response.iter_content(chunk_size=4096):
+                        
+            file.write(chunk)
+
+            loaded_content += len(chunk)
+
+            done = round((loaded_content / content_length) * 100, 2)
+
+            print(f"\rDownloading... {done}%", end="")
+            
+            
